46d3b9ce30f122dd687fc121e8db0ed9de4ac796
송채윤 <indexor18@gmail.com>
2020-02-22 23:40:49 +0900

    Update meta config

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   title: `CHAENIIZ.LOG`,
-  description: `Blog posted about ...`,
+  description: `@android @music @movie`,
   author: `chaeniiz`,
   introduction: `@Banksalad에서 안드로이드 애플리케이션을 만들고 있습니다. 음악, 영화, 공간을 사랑합니다.`,
-  siteUrl: `https://gatsby-starter-bee.netlify.com`, // Your blog site url
+  siteUrl: `https://chaeniiz.com`, // Your blog site url
   social: {
     twitter: ``, // Your Twitter account
     github: ``, // Your GitHub account
@@ -14,7 +14,7 @@ module.exports = {
   keywords: [`blog`],
   comment: {
     disqusShortName: '', // Your disqus-short-name. check disqus.com.
-    utterances: 'JaeYeopHan/gatsby-starter-bee', // Your repository for archive comment
+    utterances: 'chaeniiz/chaeniiz-blog', // Your repository for archive comment
   },
   configs: {
     countOfInitialPost: 10, // Config your initial count of post
