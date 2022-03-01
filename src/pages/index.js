4d75f440b0e73b6d8be72811f1b68cfeb1fe796f
chaeyun <indexor18@gmail.com>
2022-03-01 23:18:24 +0900

    no message

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,7 +18,7 @@ function HomePage({ data }) {
 
   return (
     <Layout>
-      <Seo title="Home" />
+      <Seo title="chaeniiz.com" />
       <Bio author={author} language={language} />
       <PostTabs
         posts={posts}
